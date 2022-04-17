461b226193726bcf67b5b3751f4163eefa711c6b
WhiteMalina <danil.bayramov.03@gmail.com>
2022-04-17 16:19:43 +0500

    0.0.3

--- Assets/Scripts/PlaceOnPlane.cs
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -1,59 +1,50 @@
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-    /// <summary>
-    /// Listens for touch events and performs an AR raycast from the screen touch point.
-    /// AR raycasts will only hit detected trackables like feature points and planes.
-    ///
-    /// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
-    /// and moved to the hit position.
-    /// </summary>
-    [RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARRaycastManager))]
     public class PlaceOnPlane : MonoBehaviour
     {
-        [SerializeField]
-        [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-        GameObject m_PlacedPrefab;
+        
+        [SerializeField] GameObject m_PlacedPrefab;
 
-    UnityEvent placementUpdate;
+        UnityEvent placementUpdate;
 
-    [SerializeField]
-    GameObject visualObject;
+        [SerializeField] GameObject visualObject;
 
-    /// <summary>
-    /// The prefab to instantiate on touch.
-    /// </summary>
-    private ARPlaneManager aRPlaneManager;
-    public GameObject placedPrefab
+        /// <summary>
+        /// The prefab to instantiate on touch.
+        /// </summary>
+
+        [SerializeField] private Camera ARCamera;
+        public Vector3 Axis { set { axis = value; } get { return axis; } } [SerializeField] private Vector3 axis = Vector3.down;
+        
+        private ARPlaneManager aRPlaneManager;
+        
+        bool IsRotation = false;
+        
+        private GameObject SelectetObject;
+        public GameObject placedPrefab
         {
             get { return m_PlacedPrefab; }
             set { m_PlacedPrefab = value; }
         }
-
-        /// <summary>
-        /// The object instantiated as a result of a successful raycast intersection with a plane.
-        /// </summary>
+ 
         public GameObject spawnedObject { get; private set; }
-
-        
         
         void Awake()
         {
             m_RaycastManager = GetComponent<ARRaycastManager>();
-           
-            
             
             if (placementUpdate == null)
                 placementUpdate = new UnityEvent();
             
-                
-                
-                placementUpdate.AddListener(DiableVisual);
+            placementUpdate.AddListener(DiableVisual);
         }
-
+        
         bool TryGetTouchPosition(out Vector2 touchPosition)
         {
             if (Input.touchCount > 0)
@@ -61,7 +52,8 @@ using UnityEngine.XR.ARSubsystems;
                 touchPosition = Input.GetTouch(0).position;
                 return true;
             }
-
+            IsRotation = false;
+            
             touchPosition = default;
             return false;
         }
@@ -73,13 +65,17 @@ using UnityEngine.XR.ARSubsystems;
 
             if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
             {
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
                 var hitPose = s_Hits[0].pose;
             
                 if (spawnedObject == null)
                 {
-                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    List<ARRaycastManager> hits = new List<ARRaycastManager>();
+                    
+                    m_RaycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), s_Hits,
+                        TrackableType.Planes);
+                    
+                    spawnedObject = Instantiate(m_PlacedPrefab, s_Hits[0].pose.position, hitPose.rotation);
+                    
                     aRPlaneManager = GetComponent<ARPlaneManager>();
 
                     aRPlaneManager.enabled = false;
@@ -88,23 +84,70 @@ using UnityEngine.XR.ARSubsystems;
                     {
                         plane.gameObject.SetActive(false);
                     }
-
-                }
-                else
-                {
-                    //repositioning of the object 
-                   spawnedObject.transform.position = hitPose.position;
                 }
                 placementUpdate.Invoke();
             }
+
+            MoveObject();
+        }
+        
+        void MoveObject()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                var touchPosition = touch.position;
+                
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = ARCamera.ScreenPointToRay(touch.position);
+                    RaycastHit hitObject;
+        
+                    if (Physics.Raycast(ray, out hitObject))
+                    {
+                        if (hitObject.collider.CompareTag("UnSelected"))
+                        {
+                            hitObject.collider.gameObject.tag = "Selected";
+                        }
+                    }    
+                }
+
+                if (Input.touchCount == 2)
+                {
+                    var finger = Lean.Touch.LeanTouch.Fingers; 
+                    var twistDegrees = Lean.Touch.LeanGesture.GetTwistDegrees(finger) * 1;
+                    Touch touch1 = Input.touches[0];
+                    Touch touch2 = Input.touches[1];
+                    
+                    m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.Planes);
+                    SelectetObject = GameObject.FindWithTag("Selected");
+                    SelectetObject.transform.Rotate(axis, twistDegrees);
+
+                    IsRotation = true;
+                }
+                
+                if (touch.phase == TouchPhase.Moved && Input.touchCount == 1 && IsRotation == false)
+                {
+                    m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.Planes);
+                    SelectetObject = GameObject.FindWithTag("Selected");
+                    SelectetObject.transform.position = s_Hits[0].pose.position;
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    if (SelectetObject.CompareTag("Selected"))
+                    {
+                        SelectetObject.tag = "UnSelected";
+                    }
+                }
+            }
         }
 
-    public void DiableVisual()
-    {
-        visualObject.SetActive(false);
-    }
+        public void DiableVisual()
+        {
+            visualObject.SetActive(false);
+        }
 
         static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
-
         ARRaycastManager m_RaycastManager;
     }
